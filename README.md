cd67579f497b394644de8bab085f99fcf203bf7e
jeremyers1 <4466264+jeremyers1@users.noreply.github.com>
2022-07-09 12:43:51 -0700

    Update JS View and Model

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # battleship
 
 Battleship Game created with Head First JavaScript
+
+Enter your board guesses on the lower right, and the game will tell you whether it was a hit or a miss. The board layout will be updated accordingly.
--- script.js
+++ script.js
@@ -1,39 +1,60 @@
-// Old Battleship code from early version
+// View: Update the User's View
+let view = {
+    displayMessage(msg) {
+        let messageArea = document.getElementById('messageArea');
+        messageArea.textContent = msg;
+    },
 
-var location1 = 3;
-var location2 = 4;
-var location3 = 5;
-var guess = -1;
-var hits = 0;
-var guesses = 0;
-var isSunk = false;
+    displayHit(loc) {
+        let cell = document.getElementById(loc);
+        cell.setAttribute('class', 'hit');
+    },
 
-
-// PROBLEM: You can guess the same HIT number over and over, and it wins.
-
-while (!isSunk) {
-  while (guess < 0 || guess > 6) {
-    guess = prompt("Ready, aim, fire! (enter a number from 0-6):");
-    if (guess < 0 || guess > 6) {
-      alert("Please enter a valid cell number!");
+    displayMiss(loc) {
+        let cell = document.getElementById(loc);
+        cell.setAttribute('class', 'miss');
     }
-  } //end while
-  guesses++;
-
-  if (guess == location1 || guess == location2 || guess == location3) {
-    alert("HIT!");
-    hits++;
- guess = -1; // to stop infinite loop
-    if (hits === 3) {
-      isSunk = true;
-      alert("You sank my battleship!");
-    }
-  } else {
-    alert("MISS");
-    guess = -1; // to stop infinite loop
-  }
 }
 
-var stats = "You took " + guesses + " guesses to sink the battleship, " +
-  "which means your shooting accuracy was " + (3 / guesses);
-alert(stats);
+// Model: Keep Track of ship location and status
+let model = {
+    boardsize: 7,
+    numShips: 3,
+    shipLength: 3,
+    shipsSunk: 0,
+
+    ships: [{ locations: ['06', '16', '26'], hits: ['', '', ''] },
+        { locations: ['24', '34', '44'], hits: ['', '', ''] },
+        { locations: ['10', '11', '12'], hits: ['', '', ''] }],
+    
+    fire(guess) {
+        for (let i = 0; i < this.numShips; i++){
+            let ship = this.ships[i];
+            let index = ship.locations.indexOf(guess);
+            if (index >= 0) { // A hit!
+                ship.hits[index] = 'hit';
+                view.displayHit(guess);
+                view.displayMessage('HIT!');
+                if (this.isSunk(ship)) {
+                    view.displayMessage('You sank my battleship!');
+                    this.shipsSunk++;
+                }
+                return true;
+            }
+        }
+        view.displayHit(guess);
+        view.displayMessage('You missed.');
+        return false;
+    },
+
+    isSunk(ship) {
+        for (let i = 0; i < this.shipLength; i++){
+            if (ship.hits[i] !== 'hit') {
+                return false;
+            }
+        }
+        return true; // the ship is sunk
+    }
+};
+
+// Controller: Control player input and game logic
